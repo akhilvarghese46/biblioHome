0951331f2539d53bf290db85ccac52001891f985
akhil <akhilvarghese46@gmail.com>
2024-05-02 19:33:35 +0100

    changes

--- controller/BookController.js
+++ controller/BookController.js
@@ -6,7 +6,7 @@ const getBooks = async (req, res) => {
   try {
    // const books = await Book.find({});
    // res.status(200).json(books);
-   res.status(200).json("hello word");
+     res.send('Hello, World!');
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
